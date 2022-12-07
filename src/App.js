163e69c63fc12839ea3dd636527e6ff30b2afe4b
Azharul-Raj <azharulislam.raj512@gmail.com>
2022-12-07 21:37:42 +0600

    remove unnecessary code from app.js

--- src/App.js
+++ src/App.js
@@ -8,43 +8,8 @@ import { authContext } from "./contexts/AuthProvider";
 
 const router = routers;
 
-
 axios.defaults.baseURL =
   'http://localhost:3001/';
-// axios.defaults.headers.common['authorization']= localStorage.getItem('token')
-// axios.interceptors.request.use(req => {
-  //   // console.log(req);
-  //   return req;
-// })
-// // Add a 401 response interceptor
-// window.axios?.interceptors.response.use(function (response) {
-  //   console.log(response.status);
-  //   if (response.status === 401 || response.status === 402 || response.status === 403) {
-    //     return console.log('status code error')
-    //   }
-//   console.log('waiting for response return');
-//   return response;
-// })
-  // function (error) {
-//   console.log('I am inside of error');
-//   if (401 === error.response.status || 402 === error.response.status || 403 === error.response.status) {
-//     console.log('401 LISTEN FROM AXIOS');
-//     toast.error('UNAUTHORIZED USER NAVIGATING TO LOGIN')
-//       // swal({
-  //       //     title: "Session Expired",
-//       //     text: "Your session has expired. Would you like to be redirected to the login page?",
-//       //     type: "warning",
-//       //     showCancelButton: true,
-//       //     confirmButtonColor: "#DD6B55",
-//       //     confirmButtonText: "Yes",
-//       //     closeOnConfirm: false
-//       // }, function(){
-  //       //     window.location = '/login';
-  //       // });
-  //   } else {
-    //       return Promise.reject(error);
-    //   }
-    // });
     
 function App() {
   const { user } = useContext(authContext);
@@ -64,11 +29,9 @@ function App() {
     // interceptors
   }
   return (
-    // <Elements stripe={stripePromise}>
     <div className="App max-w-[1440px] mx-auto">
       <RouterProvider router={router}/>
       </div>
-      // </Elements> 
   );
 }
 
