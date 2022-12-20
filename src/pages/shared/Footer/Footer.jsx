b7ba9d14e2ee5e983b7373b933a3ebd74152a7c8
Azharul-Raj <azharulislam.raj512@gmail.com>
2022-12-20 23:24:37 +0600

    image added in the footer

--- src/pages/shared/Footer/Footer.jsx
+++ src/pages/shared/Footer/Footer.jsx
@@ -1,28 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import footerImg from '../../../assets/images/footer.png';
 
 const Footer = () => {
     return (
-        <footer className="grid w-full lg:grid-cols-3 lg:place-items-stretch lg:grid-flow-col p-10 bg-base-200 text-base-content">
-  <div>
+        <footer className="flex w-full flex-col lg:justify-between lg:flex-row p-10 text-slate-700 relative">
+            <img
+          src={footerImg}
+          className="absolute inset-0 object-cover w-full h-full"
+          alt=""
+        />
+  <div className='lg:py-7'>
     <span className="footer-title">Services</span> 
-    <a className="link block link-hover">Branding</a> 
-    <a className="link block link-hover">Design</a> 
-    <a className="link block link-hover">Marketing</a> 
-    <a className="link block link-hover">Advertisement</a>
+    <Link className="link block link-hover">Emergency Checkup</Link> 
+    <Link className="link block link-hover">Monthly Checkup</Link> 
+    <Link className="link block link-hover">Weekly Checkup</Link> 
+    <Link className="link block link-hover">Deep Checkup</Link>
   </div> 
-  <div>
-    <span className="footer-title">Company</span> 
-    <a className="link block link-hover">About us</a> 
-    <a className="link block link-hover">Contact</a> 
-    <a className="link block link-hover">Jobs</a> 
-    <a className="link block link-hover">Press kit</a>
+  <div className='py-24 lg:py-7'>
+    <span className="footer-title">ORAL HEALTH</span> 
+    <Link className="link block link-hover">Fluoride Treatment</Link> 
+    <Link className="link block link-hover">Cavity Filling</Link> 
+    <Link className="link block link-hover">Teeth Whitening</Link>
   </div> 
-  <div>
-    <span className="footer-title">Legal</span> 
-    <a className="link block link-hover">Terms of use</a> 
-    <a className="link block link-hover">Privacy policy</a> 
-    <a className="link block link-hover">Cookie policy</a>
+  <div className='lg:py-7'>
+    <span className="footer-title">OUR ADDRESS</span> 
+    <Link className="link block link-hover">New York - 101010 Hudson</Link>
   </div>
 </footer>
     //     <footer className='footer p-10 bg-base-200 text-base-content'>
